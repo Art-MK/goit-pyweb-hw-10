304b181d3a0776b1d238f7564aac492f255cc1c7
Art-MK <54836473+Art-MK@users.noreply.github.com>
2024-06-30 10:01:01 +0300

    Add files via upload

--- quotes_project/scraper.py
+++ quotes_project/scraper.py
@@ -1,15 +1,14 @@
 import os
 import django
 
-# Настройка Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quotes_project.settings')
 django.setup()
 
-import scrapy
-from scrapy.crawler import CrawlerProcess
 from quotes.models import Quote, Author
+from scrapy.crawler import CrawlerProcess
+from scrapy import Spider
 
-class QuotesSpider(scrapy.Spider):
+class QuotesSpider(Spider):
     name = "quotes"
     start_urls = ['http://quotes.toscrape.com/']
 
@@ -20,16 +19,17 @@ class QuotesSpider(scrapy.Spider):
             tags = quote.css('div.tags a.tag::text').getall()
 
             author, created = Author.objects.get_or_create(fullname=author_name)
-            Quote.objects.create(text=text, author=author, tags=tags)
+            Quote.objects.get_or_create(text=text, author=author, tags=tags)
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
 
-def run_spiders():
+def run_spider():
     process = CrawlerProcess()
     process.crawl(QuotesSpider)
     process.start()
 
 if __name__ == "__main__":
-    run_spiders()
+    run_spider()
+    print("Quotes scraped successfully!")
